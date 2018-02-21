67044b6da953fc06e9f6a9a52a5740d91ca4dd39
Ashwin Km <kmuraliashwin@gmail.com>
2018-02-22 00:47:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "This is a new value";
+
+
+var img = document.getElementById("madi");
+
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+}
