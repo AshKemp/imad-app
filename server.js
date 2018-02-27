73241faa3c84f9ba9709a5f8b8a45129740bb645
Ashwin Km <kmuraliashwin@gmail.com>
2018-02-27 17:07:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ function createTemplate(data) {
         <hr>
         <h3>${heading}</h3>
         <div>
-            ${date.toDateString()}
+            ${date}
         </div>
         <div>
             ${content}
@@ -96,19 +96,9 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new Pool(config);
 
-app.get('/test-db',function(req,res) {
-   //Make a select request
-   //return a response with the response
-   pool.query("SELECT * from test",function(err,result) {
-       if(err) {
-           res.status(500).send(err.toString());
-       }else {
-           res.send(JSON.stringify(result.rows));
-       }
-   })
-});
+
+
 
 var counter = 0;
 app.get('/counter', function (req, res) {
