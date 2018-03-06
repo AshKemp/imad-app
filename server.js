529bc9260cf4b5499e74f638651ab2da9dcf8d31
Ashwin Km <kmuraliashwin@gmail.com>
2018-03-06 17:46:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-//var articles = {
+var articles = {
     'article-one' : {    
   title: 'Article One | Ashwin Km',
   heading : 'Article One',
